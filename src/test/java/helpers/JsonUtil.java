f5d5632de1f0060700c0fddafa6a237efc382545
eynar-pari <eynar_repc@hotmail.com>
2020-04-16 15:07:03 -0400

    method to compare JSONs

--- src/test/java/helpers/JsonUtil.java
+++ src/test/java/helpers/JsonUtil.java
@@ -30,4 +30,36 @@ public class JsonUtil {
         }
     }
 
+    /**
+     * This method compare two simple json
+     *
+     * @param jsonExpected it is a json. Example : {\"key1\":\"value1\",\"key2\":\"value2\"}
+     * @param jsonActual   it is a json. Example : {\"key1\":\"value1\",\"key2\":\"value2\"}
+     * @return
+     * @throws JSONException it throws is a json is invalid.
+     */
+    public static boolean areEqualsJsonObject(String jsonExpected, String jsonActual) throws JSONException {
+        boolean areEqualJsonObject = true;
+        JSONObject jsonObjectExpected = JsonUtil.convertToJSONObject(jsonExpected);
+        JSONObject jsonObjectActual = JsonUtil.convertToJSONObject(jsonActual);
+        Iterator<?> keys = jsonObjectExpected.keys();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String valueExpected = String.valueOf(jsonObjectExpected.get(key));
+            String valueActual = String.valueOf(jsonObjectActual.get(key));
+            if (valueExpected.equals("IGNORE")){
+                System.out.println("INFO>\tIgnoring.... Attribute : "+key+ " with value : "+valueActual);
+
+            } else {
+                if (!valueExpected.equals(valueActual)){
+                    System.out.println("INFO>\tProperty: [" + key + "] -> Comparing Expected Result : '" + valueExpected +
+                            "' and Actual Result : '" + valueActual + "'" + " are equal:" + valueExpected.equals(valueActual));
+
+                    areEqualJsonObject = false;
+                }
+            }
+        }
+        return areEqualJsonObject;
+    }
 }
+
